66da04e530c679c8df58a39ff93248fd2999a4cf
lopesnuno <lopesnuno@ipvc.pt>
2022-03-15 15:03:37 +0000

    Fixes
    Fixed constraints;
    Change ID types and "room_meeters" to room_members.

--- db.ddl.sql
+++ db.ddl.sql
@@ -1,52 +1,46 @@
-CREATE TABLE room
-(
-    id         varchar NOT NULL,
-    name       varchar(50),
-    created_at numeric,
-    updated_at numeric,
-    constraint pk_room PRIMARY KEY (id)
-);
-
 CREATE TABLE account
 (
-    id         varchar NOT NULL,
+    id         char(20) NOT NULL,
     name       varchar(50),
-    created_at numeric NOT NULL,
+    created_at numeric  NOT NULL,
     updated_at numeric,
     CONSTRAINT pk_account PRIMARY KEY (id)
 );
 
-CREATE TABLE room_meeters
+CREATE TABLE room
 (
-    id        varchar NOT NULL,
-    joined_at numeric,
-    CONSTRAINT pk_room_meeters PRIMARY KEY (id)
+    id         char(20) NOT NULL,
+    name       varchar(50),
+    created_at numeric NOT NULL,
+    updated_at numeric,
+    owner      char(20) NOT NULL
+        CONSTRAINT fk_room_owner REFERENCES account (id),
+    constraint pk_room PRIMARY KEY (id)
+);
+
+CREATE TABLE room_members
+(
+    id        char(20) NOT NULL,
+    joined_at numeric NOT NULL,
+    room_id   char(20) NOT NULL
+        CONSTRAINT fk_room_members_room REFERENCES room (id),
+    account_id   char(20) NOT NULL
+        CONSTRAINT fk_room_members_account REFERENCES account (id),
+    CONSTRAINT pk_room_members PRIMARY KEY (id)
 );
 
 CREATE TABLE message
 (
-    id      varchar NOT NULL,
-    send_at numeric NOT NULL,
-    message varchar(500),
+    id           char(20) NOT NULL,
+    send_at      numeric  NOT NULL,
+    message      varchar(500),
+    sender_id    char(20) NOT NULL
+        CONSTRAINT fk_message_sender REFERENCES account (id),
+    recipient_id char(20)
+        CONSTRAINT fk_message_recipient REFERENCES account (id),
+    reply_to     char(20)
+        CONSTRAINT fk_message_reply REFERENCES message (id),
+    room_id      char(20)
+        CONSTRAINT fk_message_room REFERENCES room (id),
     CONSTRAINT pk_message PRIMARY KEY (id)
 );
-
-ALTER TABLE message
-    ADD sender_id    varchar NOT NULL
-        CONSTRAINT fk_message_sender REFERENCES account (id),
-    ADD recipient_id varchar
-        CONSTRAINT fk_message_recipient REFERENCES account (id),
-    ADD reply_to     varchar
-        CONSTRAINT fk_message_reply REFERENCES message (id),
-    ADD room_id      varchar
-        CONSTRAINT fk_message_room REFERENCES room (id);
-
-ALTER TABLE room
-    ADD owner varchar NOT NULL
-        CONSTRAINT fk_room REFERENCES account (id);
-
-ALTER TABLE room_meeters
-    ADD room_id    varchar NOT NULL
-        CONSTRAINT fk_room_meeters_room REFERENCES room (id),
-    ADD account_id varchar NOT NULL
-        CONSTRAINT fk_room_meeters_account REFERENCES account (id);
--- fixtures.sql
+++ fixtures.sql
@@ -1,37 +1,26 @@
 INSERT INTO account(id, name, created_at, updated_at)
-VALUES (gen_random_uuid(), 'Joaquim', 1392223234, 1615567234);
+VALUES ('wH4ofWP4EyprKpqQWStn', 'Joaquim', 1392223234, 1615567234);
 INSERT INTO account(id, name, created_at, updated_at)
-VALUES (gen_random_uuid(), 'Rafa', 1402589434, 1647105300);
+VALUES ('6jrHnwiHihRNeWqrKirW', 'Rafa', 1402589434, 1647105300);
 INSERT INTO account(id, name, created_at, updated_at)
-VALUES (gen_random_uuid(), 'Lúcia', 1647103234, 1647103234);
-
-INSERT INTO message(id, send_at, message, sender_id, recipient_id, reply_to, room_id)
-VALUES (gen_random_uuid(), 1423759255, 'Olá!', 'a945be2b-bad6-48ae-831c-04cc809cf189', NULL, NULL,
-        '98c9f173-9252-4d49-b404-833792335608');
-INSERT INTO message(id, send_at, message, sender_id, recipient_id, reply_to, room_id)
-VALUES (gen_random_uuid(), 1423759299, 'Boas.', '99a48977-af58-4828-956b-220110c5d9db', NULL,
-        'b0eb7209-ca91-4685-8e02-c31eda44beb8', '98c9f173-9252-4d49-b404-833792335608');
-INSERT INTO message(id, send_at, message, sender_id, recipient_id, reply_to, room_id)
-VALUES (gen_random_uuid(), 1647105034, 'Olá, Joaquim!', '473961b8-af56-42c0-835d-9c3534ba7872',
-        'a945be2b-bad6-48ae-831c-04cc809cf189', NULL, NULL);
+VALUES ('hpAHdRc8jDLTMw7ikrRH', 'Lúcia', 1647103234, 1647103234);
 
 INSERT INTO room(id, name, created_at, updated_at, owner)
-VALUES (gen_random_uuid(), 'Bilcas e borgas', 1423759234, 1486917634, 'a945be2b-bad6-48ae-831c-04cc809cf189');
+VALUES ('37KQmczHRQdgKKQqhDXF', 'Biclas e borgas', 1423759234, 1486917634, 'wH4ofWP4EyprKpqQWStn');
 
-INSERT INTO room_meeters(id, joined_at, room_id, account_id)
-VALUES (gen_random_uuid(), 1423759234, '98c9f173-9252-4d49-b404-833792335608', 'a945be2b-bad6-48ae-831c-04cc809cf189');
-INSERT INTO room_meeters(id, joined_at, room_id, account_id)
-VALUES (gen_random_uuid(), 1423759289, '98c9f173-9252-4d49-b404-833792335608', '99a48977-af58-4828-956b-220110c5d9db');
-INSERT INTO room_meeters(id, joined_at, room_id, account_id)
-VALUES (gen_random_uuid(), 1647104434, '98c9f173-9252-4d49-b404-833792335608', '473961b8-af56-42c0-835d-9c3534ba7872');
+INSERT INTO room_members(id, joined_at, room_id, account_id)
+VALUES ('9q6QkDWZoToQXcKoMgFD', 1423759234, '37KQmczHRQdgKKQqhDXF', 'wH4ofWP4EyprKpqQWStn');
+INSERT INTO room_members(id, joined_at, room_id, account_id)
+VALUES ('r7GM6Aqrk2TcEnrMEdC5', 1423759289, '37KQmczHRQdgKKQqhDXF', '6jrHnwiHihRNeWqrKirW');
+INSERT INTO room_members(id, joined_at, room_id, account_id)
+VALUES ('pBobnd2YLqTFpMkJyM9t', 1647104434, '37KQmczHRQdgKKQqhDXF', 'hpAHdRc8jDLTMw7ikrRH');
 
-SELECT *
-FROM room_meeters;
-
-
-
-
-
-
-
-
+INSERT INTO message(id, send_at, message, sender_id, recipient_id, reply_to, room_id)
+VALUES ('aKoHkj6AXc2pXRkQ9mET', 1423759255, 'Olá!', 'wH4ofWP4EyprKpqQWStn', NULL, NULL,
+        '37KQmczHRQdgKKQqhDXF');
+INSERT INTO message(id, send_at, message, sender_id, recipient_id, reply_to, room_id)
+VALUES ('fm4TvoT8Woyy8SmaeKjQ', 1423759299, 'Boas.', '6jrHnwiHihRNeWqrKirW', NULL,
+        'aKoHkj6AXc2pXRkQ9mET', '37KQmczHRQdgKKQqhDXF');
+INSERT INTO message(id, send_at, message, sender_id, recipient_id, reply_to, room_id)
+VALUES ('fEXr3Zh5YjLcZa3Ats24', 1647105034, 'Olá, Joaquim!', 'hpAHdRc8jDLTMw7ikrRH',
+        'wH4ofWP4EyprKpqQWStn', NULL, NULL);
