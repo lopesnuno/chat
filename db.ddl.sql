9c4b088ab0541a2c6adca5590e84e1328a8acedd
lopesnuno <lopesnuno@ipvc.pt>
2022-03-19 14:39:05 +0000

    Fixes_2
    - Changed table names do plurar,
    - constraint -> CONSTRAINT,
    - Dates to "TIMESTANT".

--- db.ddl.sql
+++ db.ddl.sql
@@ -1,21 +1,21 @@
-CREATE TABLE account
+CREATE TABLE users
 (
     id         char(20) NOT NULL,
     name       varchar(50),
-    created_at numeric  NOT NULL,
-    updated_at numeric,
-    CONSTRAINT pk_account PRIMARY KEY (id)
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    CONSTRAINT pk_users PRIMARY KEY (id)
 );
 
-CREATE TABLE room
+CREATE TABLE rooms
 (
     id         char(20) NOT NULL,
     name       varchar(50),
-    created_at numeric NOT NULL,
-    updated_at numeric,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     owner      char(20) NOT NULL
-        CONSTRAINT fk_room_owner REFERENCES account (id),
-    constraint pk_room PRIMARY KEY (id)
+        CONSTRAINT fk_rooms_owner REFERENCES users (id),
+    CONSTRAINT pk_rooms PRIMARY KEY (id)
 );
 
 CREATE TABLE room_members
@@ -23,24 +23,24 @@ CREATE TABLE room_members
     id        char(20) NOT NULL,
     joined_at numeric NOT NULL,
     room_id   char(20) NOT NULL
-        CONSTRAINT fk_room_members_room REFERENCES room (id),
-    account_id   char(20) NOT NULL
-        CONSTRAINT fk_room_members_account REFERENCES account (id),
+        CONSTRAINT fk_room_members_rooms REFERENCES rooms (id),
+    user_id   char(20) NOT NULL
+        CONSTRAINT fk_room_members_users REFERENCES users (id),
     CONSTRAINT pk_room_members PRIMARY KEY (id)
 );
 
-CREATE TABLE message
+CREATE TABLE messages
 (
     id           char(20) NOT NULL,
     send_at      numeric  NOT NULL,
     message      varchar(500),
     sender_id    char(20) NOT NULL
-        CONSTRAINT fk_message_sender REFERENCES account (id),
+        CONSTRAINT fk_messages_sender REFERENCES users (id),
     recipient_id char(20)
-        CONSTRAINT fk_message_recipient REFERENCES account (id),
+        CONSTRAINT fk_messages_recipient REFERENCES users (id),
     reply_to     char(20)
-        CONSTRAINT fk_message_reply REFERENCES message (id),
-    room_id      char(20)
-        CONSTRAINT fk_message_room REFERENCES room (id),
-    CONSTRAINT pk_message PRIMARY KEY (id)
+        CONSTRAINT fk_messages_reply REFERENCES messages (id),
+    rooms_id      char(20)
+        CONSTRAINT fk_messages_rooms REFERENCES rooms (id),
+    CONSTRAINT pk_messages PRIMARY KEY (id)
 );
